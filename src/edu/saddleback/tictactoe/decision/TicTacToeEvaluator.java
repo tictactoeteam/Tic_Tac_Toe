39953fe538238608d61b31b98842bb3ab4aa6421
Tomasz <gruszkat98@gmail.com>
2019-01-31 09:10:04 -0800

    Changed things a bit
    Deleted WinException, the evaluator doesn't see wins yet

--- src/edu/saddleback/tictactoe/decision/TicTacToeEvaluator.java
+++ src/edu/saddleback/tictactoe/decision/TicTacToeEvaluator.java
@@ -5,7 +5,9 @@ import edu.saddleback.tictactoe.board.GamePiece;
 
 public class TicTacToeEvaluator implements StaticEvaluator {
 
+
     private int calcRowStreaks(int r, GamePiece[][] a){
+
 
         int sumX = 0;
         int sumO = 0;
@@ -24,8 +26,7 @@ public class TicTacToeEvaluator implements StaticEvaluator {
                 return 0;
         }
 
-        if (sumX == 3)
-            throw(new WinException(GamePiece.X));
+
 
         if (sumO == 2){
             if (sumX == 0)
@@ -34,12 +35,11 @@ public class TicTacToeEvaluator implements StaticEvaluator {
                 return 0;
         }
 
-        if (sumO == 3)
-            throw(new WinException(GamePiece.X));
 
         return 0;
 
     }
+
 
     private int calcColumnStreaks(int c, GamePiece[][] a){
 
@@ -60,8 +60,6 @@ public class TicTacToeEvaluator implements StaticEvaluator {
                 return 0;
         }
 
-        if (sumX == 3)
-            throw(new WinException(GamePiece.X));
 
         if (sumO == 2){
             if (sumX == 0)
@@ -70,12 +68,11 @@ public class TicTacToeEvaluator implements StaticEvaluator {
                 return 0;
         }
 
-        if (sumO == 3)
-            throw(new WinException(GamePiece.O));
 
         return 0;
 
     }
+
 
     private int calcDiagonalStreaks(GamePiece[][] a){
 
@@ -97,65 +94,71 @@ public class TicTacToeEvaluator implements StaticEvaluator {
                 numStreaks += 1;
         }
 
-        if (sumX == 3)
-            throw(new WinException(GamePiece.X));
 
         if (sumO == 2){
             if (sumX == 0)
                 numStreaks -= 1;
         }
 
-        if (sumO == 3)
-            throw(new WinException(GamePiece.O));
+
+
+
+
 
 
         if (a[2][0] ==  a[0][2]){
-            if (a[1][1] ==  a[2][0] && a[1][1] != null)
-                throw(new WinException(a[1][1]));
 
-            if (a[2][0] == GamePiece.O)
-                numStreaks -=1;
+            if (a[1][1] == null) {
+                if (a[2][0] == GamePiece.O)
+                    numStreaks -= 1;
 
-            if(a[2][0] == GamePiece.X)
-                numStreaks +=1;
+                if (a[2][0] == GamePiece.X)
+                    numStreaks += 1;
+
+            }
+
 
         }
 
 
 
-        return 0;
+        return numStreaks;
     }
 
-    // Basic evaluator. For a winning position grants 100 points, for a double streak 10 points, and for a single streak 1 point.
-    // Positive points are granted to X-player and negative to O-player
+    private GamePiece weGotTheWinner(GamePiece[][] a){
+
+        return null;
+    }
+
+
+    // Just counts the number of streaks, returns negative for O-player and positive for X-player
     @Override
     public int evaluate(Board b) {
-        final GamePiece[][] array = b.getBoard();
+        final GamePiece[][] array = b.getBoardCopy();
 
         int sumStreaks = 0;
 
-        try {
-            for (int i = 0; i < 3; ++i)
-                sumStreaks += calcRowStreaks(i, array);
 
-            for (int i = 0; i < 3; ++i)
-                sumStreaks += calcColumnStreaks(i, array);
+        if (weGotTheWinner(array) == GamePiece.X){
+            return 100;
+        }
+        else if(weGotTheWinner(array) == GamePiece.O){
+            return -100;
+        }
 
-            sumStreaks += calcDiagonalStreaks(array);
+
+        for (int i = 0; i < 3; ++i)
+            sumStreaks += calcRowStreaks(i, array);
+        for (int i = 0; i < 3; ++i)
+            sumStreaks += calcColumnStreaks(i, array);
+
+        sumStreaks += calcDiagonalStreaks(array);
 
 
-            return sumStreaks;
+        return sumStreaks;
             
 
-        }
-        catch(WinException e){
 
-            if(e.player == GamePiece.X)
-
-                return 100;
-            else
-                return -100;
-        }
 
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module Tic.Tac.Toe {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.graphics;
 
     opens edu.saddleback.tictactoe;
 }
