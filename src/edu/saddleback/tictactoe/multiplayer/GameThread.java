fd6979d762d2e4575735526d0398365f32f7fce1
Tomasz <gruszkat98@gmail.com>
2019-02-28 10:22:26 -0800

    Beginning of Server logic

--- src/edu/saddleback/tictactoe/multiplayer/GameThread.java
+++ src/edu/saddleback/tictactoe/multiplayer/GameThread.java
@@ -1,16 +1,56 @@
 package edu.saddleback.tictactoe.multiplayer;
 
+import edu.saddleback.tictactoe.decision.AdvancedEvaluator;
+import edu.saddleback.tictactoe.model.Board;
+
 import java.net.Socket;
 
 public class GameThread extends Thread {
-    private Socket socket;
 
-    public GameThread(Socket socket) {
-        this.socket = socket;
+    private GameThreadRunnable runnable;
+
+
+    public GameThread(Socket socketPlayerX, Socket socketPlayerO) {
+        runnable = new GameThreadRunnable(socketPlayerX, socketPlayerO);
     }
 
     @Override
     public synchronized void start() {
-        super.start();
+        runnable.run();
+    }
+}
+
+
+
+class GameThreadRunnable implements Runnable{
+    private Socket socketPlayerX;
+    private Socket socketPlayerO;
+    private AdvancedEvaluator winnerChecker;
+
+    GameThreadRunnable(Socket socketPlayerX, Socket socketPlayerO){
+        this.socketPlayerX = socketPlayerX;
+        this.socketPlayerO = socketPlayerO;
+        winnerChecker = new AdvancedEvaluator();
+    }
+
+    @Override
+    public void run() {
+
+        Socket currentPlayer = socketPlayerX;
+        Socket nonCurrentPlayer = socketPlayerO;
+        Socket temp;
+
+        Board board = new Board();
+        while (winnerChecker.evaluate(board) != 0 || board.getTurnNumber() != 9){
+            /**board = get board from CurrentPlayer**/
+            /**send the board to NonCurrentPlayer**/
+
+
+            System.out.println(board);
+            temp = currentPlayer;
+            currentPlayer = nonCurrentPlayer;
+            nonCurrentPlayer = temp;
+        }
+
     }
 }
--- src/edu/saddleback/tictactoe/multiplayer/Server.java
+++ src/edu/saddleback/tictactoe/multiplayer/Server.java
@@ -1,14 +1,21 @@
 package edu.saddleback.tictactoe.multiplayer;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
 
 public class Server {
     ServerSocket server;
-    private static final int PORT = 8765;
+    private static final int PORT = 6969;
+    private InetAddress PlayerX;
+    private InetAddress PlayerO;
+
+    //LinkedList<GameThread> Sessions = new LinkedList<>();
 
     public Server() {
+
         try {
             server = new ServerSocket(PORT);
         } catch (IOException e) {
@@ -22,8 +29,20 @@ public class Server {
         System.out.println("Starting server");
         while (true) {
             try {
-                Socket socket = server.accept();
-                Thread thread = new GameThread(socket);
+                System.out.println("Awaiting first player for game no.1");
+                Socket socketPlayerX = server.accept();
+                PlayerX = socketPlayerX.getInetAddress();
+                System.out.println("Player X joined the game! The Challenger's IP address is: " + PlayerX.getHostAddress());
+
+                System.out.println("Awaiting second player for game no.1");
+                Socket socketPlayerO = server.accept();
+                PlayerO = socketPlayerO.getInetAddress();
+                System.out.println("Player O joined the game! The Challenger's IP address is: " + PlayerO.getHostAddress());
+
+                System.out.println("Let the game begin!!");
+                GameThread thread = new GameThread(socketPlayerX, socketPlayerO);
+                //Sessions.add(thread);
+
                 thread.start();
             } catch (IOException e) {
                 e.printStackTrace();
