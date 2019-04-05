860161bc03b9b0106de43c2798d0d627b8a0a8e2
David Cruz <typokign1@gmail.com>
2019-04-05 13:25:30 -0700

    Revert "Checks login info, checks and creates new account."

--- src/main/java/edu/saddleback/tictactoe/view/LoginView.java
+++ src/main/java/edu/saddleback/tictactoe/view/LoginView.java
@@ -1,11 +1,10 @@
 package edu.saddleback.tictactoe.view;
 
-import edu.saddleback.tictactoe.db.PlayerDao;
-import edu.saddleback.tictactoe.model.Player;
+import edu.saddleback.tictactoe.decision.Node;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import edu.saddleback.tictactoe.controller.GameController;
-import java.sql.SQLException;
 
 /**
  * This object interacts with the login page, handles all error checking, and establishes which type of game is to be
@@ -34,47 +33,20 @@ public class LoginView {
 
     public void onLoginClicked(){
 
-        try {
-            Player tmpPlayer = PlayerDao.getPlayerByUsername(usernameTextField.getText());
+        if(true){//<-SEARCH THROUGH DATABASE IF THE USERNAME AND PASSWORD MATCH AN ACCOUNT, ELSE ERROR
 
-            if(tmpPlayer != null && tmpPlayer.checkPassword(passwordTextField.getText())){
+            //Login to the server and join the lobby, show lobby screen
 
-                //Login to the server and join the lobby, show lobby screen
+        }else{
 
-            }else{
+            errorText.setText("***error-invalid credentials***");
+            errorText.setVisible(true);
 
-                errorText.setText("***error-invalid credentials***");
-                errorText.setVisible(true);
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
 
     }
 
     public void onCreateAccountClicked(){
-
-        try {
-            Player tmpPlayer = PlayerDao.getPlayerByUsername(usernameTextField.getText());
-
-            if(tmpPlayer == null){
-
-                PlayerDao.insertPlayer(new Player(usernameTextField.getText(), passwordTextField.getText()));
-                //Login to the server and join the lobby, show lobby screen
-
-            }else{
-
-                errorText.setText("***error-account username already exists***");
-                errorText.setVisible(true);
-
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
 
         if(true) {//<-SEARCH THROUGH DATABASE AND CREATE ACCOUNT IF ACCOUNT DOES NOT EXIST
 
