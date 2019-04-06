3a60b0dfa32fa36dfe4e49073775b1cecf7cba43
Tomasz <gruszkat98@gmail.com>
2019-04-06 10:42:27 -0700

    Doing some copy/paste coding, don't really get it

--- src/main/java/edu/saddleback/tictactoe/multiplayer/BetterServer.java
+++ src/main/java/edu/saddleback/tictactoe/multiplayer/BetterServer.java
@@ -1,5 +1,11 @@
 package edu.saddleback.tictactoe.multiplayer;
 
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
 import edu.saddleback.tictactoe.messages.Request;
 import edu.saddleback.tictactoe.messages.Response;
 
@@ -13,6 +19,13 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class BetterServer {
+
+    //PN STUFF!!!
+    PubNub pubNub;
+    PNConfiguration pnConfiguration;
+
+
+
     //stays I think
     private Vector<BetterGame> games = new Vector<>();
     //stays I think, just not as a vector of *sockets*
@@ -28,7 +41,16 @@ public class BetterServer {
 
     // modifications needed-those boys will configure a PN and stuff
     public BetterServer(){
-        this(PORT);
+        this("PublishKey",  "SubscriberKey");
+    }
+
+    public BetterServer(String pubKey, String subKey){
+        pnConfiguration = new PNConfiguration();
+        pnConfiguration.setPublishKey(pubKey);
+        pnConfiguration.setSubscribeKey(subKey);
+        pnConfiguration.setSecure(false);
+
+        pubNub = new PubNub(pnConfiguration);
     }
 
     public BetterServer(int port){
@@ -47,15 +69,79 @@ public class BetterServer {
 
 
     // function that adds a client
-    public Socket acceptConnection(){
-        try {
-            Socket client = serverSocket.accept();
-            connections.add(client);
-            return client;
-        }catch(IOException ex){
-            System.out.println("Unable to establish the connection!");
-            return null;
-        }
+    public void acceptConnection(){
+        pubNub.addListener(new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+                if (status.getOperation() != null) {
+                    switch (status.getOperation()) {
+                        // let's combine unsubscribe and subscribe handling for ease of use
+                        case PNSubscribeOperation:
+                        case PNUnsubscribeOperation:
+                            // note: subscribe statuses never have traditional
+                            // errors, they just have categories to represent the
+                            // different issues or successes that occur as part of subscribe
+                            switch (status.getCategory()) {
+                                case PNConnectedCategory:
+                                    // this is expected for a subscribe, this means there is no error or issue whatsoever
+                                case PNReconnectedCategory:
+                                    // this usually occurs if subscribe temporarily fails but reconnects. This means
+                                    // there was an error but there is no longer any issue
+                                case PNDisconnectedCategory:
+                                    // this is the expected category for an unsubscribe. This means there
+                                    // was no error in unsubscribing from everything
+                                case PNUnexpectedDisconnectCategory:
+                                    // this is usually an issue with the internet connection, this is an error, handle appropriately
+                                case PNAccessDeniedCategory:
+                                    // this means that PAM does allow this client to subscribe to this
+                                    // channel and channel group configuration. This is another explicit error
+                                default:
+                                    // More errors can be directly specified by creating explicit cases for other
+                                    // error categories of `PNStatusCategory` such as `PNTimeoutCategory` or `PNMalformedFilterExpressionCategory` or `PNDecryptionErrorCategory`
+                            }
+
+                        case PNHeartbeatOperation:
+                            // heartbeat operations can in fact have errors, so it is important to check first for an error.
+                            // For more information on how to configure heartbeat notifications through the status
+                            // PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+                            if (status.isError()) {
+                                // There was an error with the heartbeat operation, handle here
+                            } else {
+                                // heartbeat operation was successful
+                            }
+                        default: {
+                            // Encountered unknown status type
+                        }
+                    }
+                } else {
+                    // After a reconnection see status.getCategory()
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                String messagePublisher = message.getPublisher();
+                System.out.println("Message publisher: " + messagePublisher);
+                System.out.println("Message Payload: " + message.getMessage());
+                System.out.println("Message Subscription: " + message.getSubscription());
+                System.out.println("Message Channel: " + message.getChannel());
+                System.out.println("Message timetoken: " + message.getTimetoken());
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+
+//        try {
+//            Socket client = serverSocket.accept();
+//            connections.add(client);
+//            return client;
+//        }catch(IOException ex){
+//            System.out.println("Unable to establish the connection!");
+//            return null;
+//        }
     }
 
     // function that handles a message, I think in PN it's together when you add a client, so this should go
@@ -76,9 +162,9 @@ public class BetterServer {
 
         while(true) {
             System.out.println(">>>Waiting for a connection...");
-            Socket connection = acceptConnection();
-            System.out.println(">>>Connection established: " + connection.getInetAddress());
-            connectionHandlers.execute(generateConnectionHandler(connection));
+            acceptConnection();
+            System.out.println(">>>Connection established");
+            //connectionHandlers.execute(generateConnectionHandler(connection));
         }
 
     });
