d0c6c28fc3c4b1fc6e776bcf3cde254bfc08f94c
Tomasz <gruszkat98@gmail.com>
2019-04-23 11:27:31 -0700

    Login puts the user to the hashmap

--- src/main/java/edu/saddleback/tictactoe/multiplayer/handlers/LoginHandler.java
+++ src/main/java/edu/saddleback/tictactoe/multiplayer/handlers/LoginHandler.java
@@ -40,7 +40,7 @@ public class LoginHandler implements MessageHandler {
                 return;
             }
 
-
+            server.addToUsers(data.get("UUID").getAsString(), username);
             sendLoggedIn(pubnub, username);
         } catch (SQLException e) {
             e.printStackTrace();
